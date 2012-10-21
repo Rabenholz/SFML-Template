df5c3cc1eba0ae2e480f6f76cd528ef593065142
Elliot Hatch <elliot.hatch@gmail.com>
2012-10-21 17:30:07 -0600

    Organized assets

--- SFML_Template/SFMLApp_OnInit.cpp
+++ SFML_Template/SFMLApp_OnInit.cpp
@@ -10,23 +10,23 @@ bool SFMLApp::OnInit(void)
 	m_mainWindow.create(sf::VideoMode(800, 600, 32), "SFMLApp Window");
 	//initialize images
 	TextureManager& textureManager = TextureManager::getInstance();
-	textureManager.addTextureFromFile("testImage.bmp", "testImage");
-	textureManager.addTextureFromFile("TicTacToeX.bmp", "TicTacToeX");
-	textureManager.addTextureFromFile("TicTacToeO.bmp", "TicTacToeO");
-	textureManager.addTextureFromFile("boyRunning0.png", "boyRunning0");
-	textureManager.addTextureFromFile("boyRunning1.png", "boyRunning1");
-	textureManager.addTextureFromFile("boyRunning2.png", "boyRunning2");
-	textureManager.addTextureFromFile("boyRunning3.png", "boyRunning3");
+	textureManager.addTextureFromFile("assets/graphics/testImage.bmp", "testImage");
+	textureManager.addTextureFromFile("assets/graphics/TicTacToeX.bmp", "TicTacToeX");
+	textureManager.addTextureFromFile("assets/graphics/TicTacToeO.bmp", "TicTacToeO");
+	textureManager.addTextureFromFile("assets/graphics/boyRunning0.png", "boyRunning0");
+	textureManager.addTextureFromFile("assets/graphics/boyRunning1.png", "boyRunning1");
+	textureManager.addTextureFromFile("assets/graphics/boyRunning2.png", "boyRunning2");
+	textureManager.addTextureFromFile("assets/graphics/boyRunning3.png", "boyRunning3");
 
 	SpriteManager& spriteManager = SpriteManager::getInstance();
 	spriteManager.addSprite(sf::Sprite(textureManager.getTexture("testImage")),"testImage");
 	spriteManager.addSprite(sf::Sprite(textureManager.getTexture("TicTacToeX"),sf::IntRect(30,30,60,60)),"TicTacToeX_Cut");
 
 	SoundManager& soundManager = SoundManager::getInstance();
-	soundManager.addSoundBufferFromFile("bro.wav", "bro");
+	soundManager.addSoundBufferFromFile("assets/sounds/bro.wav", "bro");
 
 	FontManager& fontManager = FontManager::getInstance();
-	fontManager.addFontFromFile("impact.ttf", "Impact");
+	fontManager.addFontFromFile("/assets/fonts/impact.ttf", "Impact");
 
 	//test - register gameStates
 	std::unique_ptr<GameState_StartMenu> menuState(new GameState_StartMenu(m_mainWindow));
